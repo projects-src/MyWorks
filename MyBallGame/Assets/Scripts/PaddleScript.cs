fa37da1b72b945c406f480e42921510c95bf421c
m-kinugawa <example.com>
2019-01-23 15:29:56 +0900

    add folder

---
+++ MyBallGame/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        //speed = 5f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0f, 0f) ;
+        //Debug.Log(transform.position.x);
+	}
+}
